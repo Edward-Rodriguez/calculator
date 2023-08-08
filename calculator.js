983be0b277c9173bfcb4e55a547b7e5542bf4eca
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-08-08 13:21:35 -0400

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +16,23 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case 'add':
+      add(num1, num2);
+      break;
+    case 'subtract':
+      subtract(num1, num2);
+      break;
+    case 'multiply':
+      multiply(num1, num2);
+      break;
+    case 'divide':
+      divide(num1, num2);
+      break;
+    default:
+      break;
+  }
 }
